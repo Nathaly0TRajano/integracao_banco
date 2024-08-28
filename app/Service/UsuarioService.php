1c51d614c485ba558b46ed38ae07f5a25deeb662
Nathaly0TRajano <nathydelima00@gmail.com>
2024-08-28 15:26:15 -0300

    aula dia 28-08-2024

--- app/Service/UsuarioService.php
+++ app/Service/UsuarioService.php
@@ -19,24 +19,100 @@ class UsuarioService
      
     }
 
-    public function update(){
+    public function update(array $dados){
+         $usuario = Usuario::find($dados ['id']);
+         if ($usuario == null){
+            return [
+                'status'=> false,
+                'message'=> 'Usuario não encontrado'
+            ];
+         }
+         if (isset($dados['nome'])){
+         $usuario->nome = $dados['nome'];
+         }
+         if (isset($dados['email'])){
+         $usuario->email = $dados['email'];
+         }
+         if (isset($dados['password'])){
+         $usuario->password = $dados['password'];
+        }
+        
+         $usuario->save();
+         return [
+            'status'=> true,
+            'massage'=> 'Usuario atualizado com sucesso'
+         ];
 
     }
 
-    public function delete(){
-
+    public function delete($id){
+        $usuario = Usuario::find($id);
+        if ($usuario == null){
+            return [
+                'status'=> false,
+                'message'=> 'Usuario não encontrado'
+            ];
+        }
+        $usuario->delete();
+        return [
+            'status'=> true,
+            'message'=> 'O usuario foi excluído com sucesso'
+        ];
     }
 
-    public function findById(){
-
+    public function findById($id){
+        $usuario = Usuario::find($id);
+        if ($usuario == null ){
+        return [
+            'status' =>false,
+            'message' => 'Usuario não encontrado'
+                ];
+        } 
+        return [
+        'status' =>true,
+        'message' => 'Usuario encontrado',
+        'data' => $usuario
+    ];
     }
     // Buscar o registro, vai aparecer tudo o que tiver na tabela.
+    
     public function getALL(){
-
+        $usuarios = Usuario::all();
+        return [
+            'status'=> true,
+            'message'=> 'Pesquisa efetuada com sucesso',
+            'data' => $usuarios
+        ];
     }
 
-    public function searchByName(){
+    public function searchByName($nome){
+        $usuarios = Usuario::where('nome', 'like', '%' . $nome . "%")->get();
+        if($usuarios->isEmpty()){
+            return [
+                'statu'=> false,
+                'message' => 'Sem resultado',
+            ];
+        }
+        return [
+            'status' => true,
+            'message' => 'Resultados Encontrados',
+            'data' => $usuarios
+        ];
+    }
 
+    public function searchByEmail($email){
+        $usuarios = Usuario::where('email', 'like', '%' . $email . '%')->get();
+        if($usuarios->isEmpty()){
+            return [
+                'status' => false,
+                'message' => 'Sem resultado'
+            ];
+        }
+        return [
+            'status' => true,
+            'message'=> "Resultado encontrado",
+            'data' => $usuarios
+        ];
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,21 @@ use Illuminate\Support\Facades\Route;
 
 // UsuarioController é uma classe que nós criamos, ela está sendo criada na aba de UsuarioController.php
 Route::post('/usuario', [UsuarioController::class, 'store']);
+
+Route::get('/usuario/{id}/find',
+[UsuarioController::class, 'findById']);
+
+Route::get('/usuario',
+[UsuarioController::class, 'index']);
+
+Route::get('/usuario/search',
+[UsuarioController::class, 'searchByName']);
+
+Route::get('/usuario/email',
+[UsuarioController::class, 'searchByEmail']);
+
+Route::delete('/usuario/{id}/delete',
+[UsuarioController::class, 'delete']);
+
+Route::put('/usuario',
+[UsuarioController::class, 'update']);
